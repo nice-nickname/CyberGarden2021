8fb626a7af39ae986abfa37fa80a7f0da298a1ff
Dmitry Yachmentsev <yachmentsev@list.ru>
2021-05-29 22:09:49 +0300

    main page beta

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,34 @@
-const header = document.querySelector(".header")
+const header = document.querySelector(".mainHeader")
 const body = document.querySelector("body")
 const btnStart = document.querySelector(".main__btnStart")
-const mainSection = document.querySelector(".main")
+const mainSection = document.querySelector(".main__section")
 const logoImg = document.querySelector(".logo__img")
-
+const textWelcome = document.querySelector(".textWelcome")
+const logoBig = document.querySelector(".main__logoBig")
 btnStart.addEventListener("click", () => {
-    // header.style.paddingBottom = "34px";
+    header.style.paddingBottom = "34px";
+    textWelcome.style.display = "none";
     btnStart.style.display = "none";
+    mainSection.style.display = "flex";
+    logoBig.style.display = "none";
+    let logoBlock = document.createElement('div');
+    logoBlock.innerHTML = `<div class="logo__blockImg">
+                                <img class="logo__img" src="static/image/logo.png" alt="Логотип">
+                            </div>
+                            <h1 class="logo__title">project taganrog.arg</h1>
+                           `
+    logoBlock.classList.add('logo')
+    header.appendChild(logoBlock)
+
+
     let regForm = document.createElement('div');
     regForm.innerHTML = `
-                              <section class="main">
                                 <p class="login__title">Для продолжения <br>введите Вашу почту:</p>
                                 <input class="login__input" type="text" placeholder="example@.ru">
                                 <button class="login__Btn">Войти</button>
-                              </section>
                         `;
     regForm.classList.add('login');
-    body.appendChild(regForm);
+    mainSection.appendChild(regForm);
 
     // logoImg.style.width = "75%";
     // logoImg.style.height = "80%";
