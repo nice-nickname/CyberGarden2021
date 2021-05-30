15ba5cb43965e97243e848a05b8c5e1bf599d0b7
Dmitry Yachmentsev <yachmentsev@list.ru>
2021-05-30 06:02:09 +0300

    separation

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector(".mainHeader")
 const body = document.querySelector("body")
 const btnStart = document.querySelector(".main__btn")
-const mainSection = document.querySelector(".main   ")
+const mainSection = document.querySelector(".main")
 const logoImg = document.querySelector(".logo__img")
 const textWelcome = document.querySelector(".main__text")
 const logoBig = document.querySelector(".main__logoBig")
@@ -13,27 +13,27 @@ function change() {
     btnStart.style.display = "none";
 }
 
-btnStart.addEventListener("click", () => {
-    change()
-
-    let logoBlock = document.createElement('div');
-    logoBlock.innerHTML = `<div class="logo__blockImg">
-                                <img class="logo__img" src="static/image/logo.png" alt="Логотип">
-                            </div>
-                            <h1 class="logo__title">project taganrog.arg</h1>
-                           `
-    logoBlock.classList.add('logo')
-    header.appendChild(logoBlock)
-
-    let regForm = document.createElement('div');
-    regForm.innerHTML = `
-                                <p class="login__title">Для продолжения <br>введите Вашу почту:</p>
-                                <input class="login__input" type="text" placeholder="example@.ru">
-                                <button class="login__Btn">Войти</button>
-                        `;
-    regForm.classList.add('login');
-    mainSection.appendChild(regForm);
-})
+// btnStart.addEventListener("click", () => {
+//     change()
+//
+//     let logoBlock = document.createElement('div');
+//     logoBlock.innerHTML = `<div class="logo__blockImg">
+//                                 <img class="logo__img" src="static/image/logo.png" alt="Логотип">
+//                             </div>
+//                             <h1 class="logo__title">project taganrog.arg</h1>
+//                            `
+//     logoBlock.classList.add('logo')
+//     header.appendChild(logoBlock)
+//
+//     let regForm = document.createElement('div');
+//     regForm.innerHTML = `
+//                                 <p class="login__title">Для продолжения <br>введите Вашу почту:</p>
+//                                 <input class="login__input" type="text" placeholder="example@.ru">
+//                                 <button class="login__Btn">Войти</button>
+//                         `;
+//     regForm.classList.add('login');
+//     mainSection.appendChild(regForm);
+//})
 
 // setTimeout(() => {
 //     let inputLogin = document.querySelector(".login__input");
